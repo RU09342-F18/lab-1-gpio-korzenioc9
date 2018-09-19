2a130385fb60eddca007168dce4de5425e09dfb1
korzenioc9 <36169270+korzenioc9@users.noreply.github.com>
2018-09-19 16:48:47 -0400

    Update MSP430G2553.c

--- Off_Board Blink/MSP430G2553.c
+++ Off_Board Blink/MSP430G2553.c
@@ -1,1 +1,40 @@
+#include <msp430.h>
+
+int main() {
+
+    WDTCTL = WDTPW + WDTHOLD; //STOP WATCHDOG TIMER
+
+    P1SEL &= ~BIT0; //CONFIGURES PIN 1.0 AS AN I/O PORT
+    P1SEL2 &= ~BIT0;
+    P1DIR |= BIT0;
+
+    P1SEL &= ~BIT6; //CONFIGURES PIN 1.6 AS AN I/O PORT
+    P1SEL2 &= ~BIT6;
+    P1DIR |= BIT6;
+
+    volatile unsigned int n, i;
+
+    i = 10000;
+    n = 50000;
+
+    for (;;)
+    {
+
+    n--;
+    i--;
+
+    if (n==0)
+    {
+        P1OUT ^= BIT0;
+        n = 50000;
+    }
+    if (i==0)
+    {
+    P1OUT ^= BIT6;
+    i = 10000;
+    }
+    }
+
+return(0);
+}
 
