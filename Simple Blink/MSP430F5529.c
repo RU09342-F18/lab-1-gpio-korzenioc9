bc856a7ae5e3821e7c44bffd4501cf2927f971bd
korzenioc9 <36169270+korzenioc9@users.noreply.github.com>
2018-09-21 16:52:51 -0400

    Update MSP430F5529.c

--- Simple Blink/MSP430F5529.c
+++ Simple Blink/MSP430F5529.c
@@ -1,13 +1,18 @@
+/*
+*created by Cameron Korzeniowski
+*9/11/2018
+*/
+
 #include <msp430.h>
 
 int main(void) {
 WDTCTL = WDTPW | WDTHOLD; //stop watchdog timer
-P1SEL &= ~BIT0;
-P1DIR |= BIT0;
+P1SEL &= ~BIT0; // sets P1DIR to handle GPIO
+P1DIR |= BIT0; // Sets direction of P1DIR as output
 while(1)
 {
-P1OUT ^= BIT0;
-_delay_cycles(100000);
+P1OUT ^= BIT0; // toggles P1OUT to blink the LED
+_delay_cycles(100000); // determines the blink rate of the LED
 }
 return 0;
 }
