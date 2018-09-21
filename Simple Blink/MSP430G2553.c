6c9b804ebd43f61dcae7023a530002c348a647c1
korzenioc9 <36169270+korzenioc9@users.noreply.github.com>
2018-09-21 16:51:03 -0400

    Update MSP430G2553.c

--- Simple Blink/MSP430G2553.c
+++ Simple Blink/MSP430G2553.c
@@ -1,13 +1,18 @@
+/*
+*created by Cameron Korzeniowski
+*9/11/2018
+*/
+
 #include <msp430.h>
 
 int main() {
     WDTCTL = WDTPW | WDTHOLD; // STOP WATCHDOG TIMER
-    P1SEL &= ~BIT0;           //
-    P1SEL2 &= ~BIT0;           //
-    P1DIR |= BIT0;            //
+    P1SEL &= ~BIT0;           // setting P1SEL and P1SEL2 to 0 allows P1DIR to perform I/O operations
+    P1SEL2 &= ~BIT0;           
+    P1DIR |= BIT0;            // sets P1DIR to 1, making it an output
 while(1){                     //
-    P1OUT ^= BIT0;            //
+    P1OUT ^= BIT0;            // toggle the value of P1OUT to blink the LED
 __delay_cycles(1000000);      // This line of code 'distracts' the processor with meaningless tasks to reduce the LED blink rate.
-    }
+    }                         // The higher the number, the longer the delay and blink rate
 return(0);
 }
